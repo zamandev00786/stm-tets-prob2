f12fc94dba2ef0076d7c79eda30481069acb6bfa
zaman <developmentwithzaman00786@gmail.com>
2026-07-29 14:34:38 +0500

    crud operation added for student
    Reffering to problem 2 code is implemented

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,36 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const result = await getAllStudents(req.query);
+    res.status(200).json(result);
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const result = await addNewStudent(req.body);
+    res.status(201).json(result);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = {
+        ...req.body,
+        userId: req.params.id
+    };
+    const result = await updateStudent(payload);
+    res.status(200).json(result);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const result = await getStudentDetail(id);
+    res.status(200).json(result);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const { status } = req.body;
+    const reviewerId = req.user.id;
+    const result = await setStudentStatus({ userId: id, reviewerId, status });
+    res.status(200).json(result);
 });
 
 module.exports = {
